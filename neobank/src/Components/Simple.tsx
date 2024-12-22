237a91829b039b8429ba9848504d02cf37fda698
furkanguvenme <furkanguven.me@gmail.com>
2024-12-22 18:56:16 +0300

    Change Router Version

---
+++ neobank/src/Components/Simple.tsx
@@ -0,0 +1,15 @@
+import { Footer } from "../Layers/Footer"
+import { Header } from "../Layers/Header"
+
+
+export const Simple = () => {
+  return (
+    <>
+        <div>
+            <Header/>
+            Simple
+        </div>
+        <Footer/>
+    </>
+  )
+}
