ff7853dc2b3d8594da2080bd4462242283f1450d
furkanguvenme <furkanguven.me@gmail.com>
2025-01-03 15:44:30 +0300

    Text Change

--- neobank/src/Layers/Footer.tsx
+++ neobank/src/Layers/Footer.tsx
@@ -30,8 +30,9 @@ export const Footer = () => {
             {showAlert && (
               <div className="fixed inset-0 flex items-center justify-center bg-black/50">
                 <div className="w-4/5 lg:w-1/5 bg-white p-6 rounded-lg shadow-lg text-center">
-                  <p className="text-lg mb-4">
-                  Yapacağınız hesaplamalar ve verdiğim bilgiler, araştırmam doğrultusunda elde ettiğim bilgiler ile hazırlanmıştır. Ancak, sağlanan bilgilerin kesin doğruluğunu garanti edememekteyim. Lütfen önemli kararlar almadan önce bu bilgileri doğrulamayı ihmal etmeyiniz. Ayrıca, bu bilgilerden kaynaklanabilecek herhangi bir hata, eksiklik veya yanlış anlaşılma durumunda hiçbir şekilde sorumluluk veya yükümlülük kabul etmediğimi belirtmek isterim.
+                  <p className="text-lg mb-4 text-left">
+                  Yapacağınız hesaplamalar ve verdiğim bilgiler, araştırmam doğrultusunda elde ettiğim verilere dayanmaktadır. Ancak, sağlanan bilgilerin kesin doğruluğunu garanti edememekteyim.<br/><br/>
+Lütfen önemli kararlar almadan önce bu bilgileri doğrulamayı ihmal etmeyiniz. Ayrıca, bu bilgilerden kaynaklanabilecek herhangi bir hata, eksiklik veya yanlış anlaşılma durumunda hiçbir şekilde sorumluluk veya yükümlülük kabul etmediğimi belirtmek isterim.
                   </p>
                   <button
                     onClick={closeAlert}
